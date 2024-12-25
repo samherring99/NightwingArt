2f9432e534c96da3eadf90799857aeb70258ab9b
Sam Herring <samherring99@gmail.com>
2024-12-24 17:47:26 -0800

    Initial tool commit with samples

---
+++ tools/pixel_sort.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import numpy as np
+
+def pixel_sort(image_path):
+    img = Image.open(image_path)
+    pixels = np.array(img)
+
+    for i in range(pixels.shape[0]):
+        row = pixels[i]
+        # Sort based on pixel brightness
+        row_sorted = sorted(row, key=lambda pixel: np.mean(pixel))
+        pixels[i] = row_sorted
+
+    result_img = Image.fromarray(pixels)
+    result_img.save("outputs/test1.jpg")
+
+pixel_sort("inputs/test1.jpg")
